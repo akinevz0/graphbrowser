af7c54685cc2733725590f41d814d8135e7a1ad5
Kirill kine Nevzorov <akinevz@outlook.com>
2025-03-22 21:13:18 +0000

    add quarkus resources

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,5 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+create table IF NOT EXISTS ResourceReference (url varchar(255) not null, primary key(url));
