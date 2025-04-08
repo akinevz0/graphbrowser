8de6a0b9d5a2eb5c014efc92abf9b5863798e22e
Kirill kine Nevzorov <akinevz@outlook.com>
2025-04-08 17:53:13 +0000

    remove transactional code

--- src/main/java/ac/uk/k253/graphbrowser/beans/PagerTS.java
+++ src/main/java/ac/uk/k253/graphbrowser/beans/PagerTS.java
@@ -4,15 +4,10 @@ import java.beans.JavaBean;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
-import org.jboss.logging.Logger;
-
-import ac.uk.k253.graphbrowser.entities.ViewedResource;
 import ac.uk.k253.graphbrowser.entities.dto.PagertsDTO;
 import ac.uk.k253.graphbrowser.exceptions.NoSuchProgramException;
 import ac.uk.k253.graphbrowser.exceptions.PagerRunException;
 import jakarta.enterprise.inject.Default;
-import jakarta.transaction.Transactional;
-import jakarta.transaction.Transactional.TxType;
 
 @JavaBean
 @Default
@@ -29,22 +24,7 @@ public class PagerTS implements AutoCloseable  {
         pagerts.close();
     }
 
-    @Transactional(value = TxType.REQUIRES_NEW)
-    public ViewedResource view(final String url) {
-        Logger.getLogger(getClass()).debug("invoking pagerts.view() on " + url);
-        // viewing a page
-        final var cached = ViewedResource.locate(url);
-        // if cached and in database nothing else needed
-        if (cached.isPresent())
-            return cached.get();
-
-        // if remote then locate
-        final var dto = intoDto(url);
-        final var viewed =  dto.toViewedResource();
-        return viewed;
-    }
-
-    PagertsDTO intoDto(final String url) {
+    public PagertsDTO intoDto(final String url) {
         try {
             final var result = pagerts.runWithArgs(url).get();
             final var statusCode = result.getStatusCode();
