4b6935c490bb78aee984ed1bacd9805bbe44ca7e
Kirill N <akinevz@outlook.com>
2025-07-21 12:40:08 +0000

    chmod + chown local user to fix windows partitioning

--- src/main/webui/src/components/HomePage.tsx
+++ src/main/webui/src/components/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePageComponent = () => {
   const [state, setState] = useState<string|null>(null)
   useEffect(() => {
     (async () => {
-      const response = await fetch("/src/index.md")
+      const response = await fetch("index.md")
       const text = await response.text()
       setState(text)
     })()
